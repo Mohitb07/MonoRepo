ca22f2bb9c9acfd0356b6fec7fc0a04db651f965
mohitb07 <bmohit980@gmail.com>
2022-07-03 13:38:19 +0530

    updated

--- apps/myfirstnx/pages/api/mohit.ts
+++ apps/myfirstnx/pages/api/mohit.ts
@@ -1,5 +1,10 @@
-export default function handler(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ name: 'John Doe' });
-  }
+import express from 'express';
+const app = express()
+
+app.get('/api/mohit', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.send({message: "Hello world"})
+});
+
+module.exports = app;
