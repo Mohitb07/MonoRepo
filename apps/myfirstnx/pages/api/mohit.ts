a55a8946f600a8786807fa2f488ecadf0b6b882b
mohitb07 <bmohit980@gmail.com>
2022-07-03 13:25:04 +0530

    updated

--- apps/myfirstnx/pages/api/mohit.ts
+++ apps/myfirstnx/pages/api/mohit.ts
@@ -1,23 +1,5 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
- import express from 'express';
- import * as cors from 'cors';
- const app = express();
- 
- app.use(cors())
- 
- app.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.send({message: 'Hello world'})
-  });
- 
- const port = 3333;
- const server = app.listen(port, () => {
-   console.log(`Listening at https://mono-repo-ashy.vercel.app/`);
- });
- server.on('error', console.error);
- 
+export default function handler(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ name: 'John Doe' });
+  }
--- apps/myfirstnx/pages/mohit.tsx
+++ apps/myfirstnx/pages/mohit.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const Mohit:React.FC = () => {
     useEffect(() => {
-        fetch('https://mono-repo-ashy.vercel.app/3333/api/mohit')
+        fetch('/api/mohit')
         .then(res => res.json())
         .then(result => console.log('response', result))
     }, [])
